03940a2dc36322b53335938471e0e703559c9ed7
LeDevDuDimanche <test@anon.com>
2021-05-19 11:46:21 +0200

    add some comments for the ApiExploreStatisticsReponse class (reference intervals)

--- src/app/models/api-response-models/explore-statistics/explore-statistics-response.ts
+++ src/app/models/api-response-models/explore-statistics/explore-statistics-response.ts
@@ -5,22 +5,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
+//This class describes an interval from a histogram. 
 export class ApiInterval {
-  encCount: string
+  encCount: string // the encrypted count of observations that fall within this interval
   higherBound: string
   lowerBound: string
 }
 
+//describes a histogram received from the backend after an explore-statistics request
 export class ApiExploreStatisticsResponse {
 
   intervals: ApiInterval[]
 
-  unit: string
+  unit: string // the unit of the x-axis of the histogram
 
   timers: {
     name: string
     milliseconds: number
   }[]
-
-
 }
